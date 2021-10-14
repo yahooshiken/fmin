261502d25167a5891d2d2477f56c37b982f4ee98
Hoshi <hoshimure.47@gmail.com>
2021-10-15 01:04:01 +0900

    :tada: Dart create.

---
+++ test/fmin_test.dart
@@ -0,0 +1,16 @@
+import 'package:fmin/fmin.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
